809a2d7b6b6954d60e65d62c64da7ad08f37b2be
Ashwin <ashwintheagarajan@gmail.com>
2023-12-27 22:02:47 +1100

    Trying to fix movement (messed up refactoring)

--- Scripts/testPlayer.cs
+++ Scripts/testPlayer.cs
@@ -1,58 +1,24 @@
 using Godot;
 using System;
 
-/*public partial class testPlayer : CharacterBody2D
-{
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
-
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector2 velocity = Velocity;
-
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
-
-		Velocity = velocity;
-		MoveAndSlide();
-	}
-}*/
 
 public partial class testPlayer : CharacterBody2D
 {
 	[Export]
-    public int Speed { get; set; } = 10000;
-	public int Gravity { get; set; } = 50000;
+    public int Speed { get; set; } = 2000;
+	public int Gravity { get; set; } = 20000;
 	public float Friction { get; set; } = 0.1f;
 	public float Acceleration { get; set; } = 0.5f;
-	public int JumpHeight { get; set; } = 3000;
+	public int JumpHeight { get; set; } = 5000;
 	public float JumpProgress { get; set; } = 0f;
 	public bool IsJumping { get; set; } = false;
 	public float FallProgress { get; set; } = 0f;
 	//public bool isFalling { get; set; } = false;
 
+	public Vector2 velocity = new Vector2();
 
 	public void PlayerJump(float delta, Vector2 velocity) {
+		GD.Print("JumpProgress: " + delta);
 		JumpProgress += delta;
         float t = 0.25f - JumpProgress;
         velocity.Y = -JumpHeight * (0.25f - (0.25f - t));
@@ -60,9 +26,10 @@ public partial class testPlayer : CharacterBody2D
 			IsJumping = false;
 			FallProgress = 0;
 		}
+		return;
 	}
 
-	public void PlayerMove(float delta, Vector2 velocity, int direction) {
+	public void PlayerMove(float delta, int direction) {
 		if (direction != 0) {
 			//GD.Print("direction: " + direction);
 			velocity.X = Mathf.Lerp(velocity.X, direction * Speed, Acceleration * delta);
@@ -70,16 +37,18 @@ public partial class testPlayer : CharacterBody2D
 		else {
 			velocity.X = Mathf.Lerp(velocity.X, 0, Friction * delta);
 		}
+		return;
 	}
 
-	public void PlayerFall(float delta, Vector2 velocity) {
+	public void PlayerFall(float delta) {
 		FallProgress += delta;
 		velocity.Y += FallProgress * Gravity * delta;
+		return;
 	}
 
     public void GetInput(float delta) {
+		velocity = Velocity;
 
-		Vector2 velocity = new Vector2();
 		int direction = 0; 
 		if (Input.IsActionPressed("left")) {
 			direction -= 1;
@@ -87,7 +56,7 @@ public partial class testPlayer : CharacterBody2D
 		if (Input.IsActionPressed("right")) {
 			direction += 1;
 		}		
-		PlayerMove(delta, velocity, direction);
+		PlayerMove(delta, direction);
 		// jump
 		if (Input.IsActionJustPressed("up") && (IsOnFloor() || IsOnWall()) && !IsJumping) {
         	IsJumping = true;
@@ -99,7 +68,7 @@ public partial class testPlayer : CharacterBody2D
 			GD.Print("JumpProgress: " + JumpProgress + " velocity.Y: " + velocity.Y);
 		} else {
 			// gravity
-			PlayerFall(delta, velocity);
+			PlayerFall(delta);
 		}
     	Velocity = velocity;
 
@@ -114,6 +83,7 @@ public partial class testPlayer : CharacterBody2D
 		// // gravity
 		// velocity.Y += Gravity * delta;
 		// Velocity = velocity;
+		return;
     }
 
     public override void _PhysicsProcess(double delta) {
