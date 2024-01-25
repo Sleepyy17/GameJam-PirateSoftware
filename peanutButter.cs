cd0dc67f187ff7efde4ab2c165d1671b7e56c2b6
vin0171 <vincent0610047@gmail.com>
2024-01-25 15:02:23 +1100

    Peanut Butter: Sprite Changes According to Texture
    Sprite Changes According to Texture

---
+++ peanutButter.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public partial class peanutButter : Node2D
+{
+	private Texture2D knifeWithButter;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		knifeWithButter = (Texture2D)ResourceLoader.Load("res://Assets/knifeButteredUp.png");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void _on_area_2d_body_entered(CharacterBody2D body)
+	{	
+		
+		Sprite2D knifeSprite = body.GetNode("KnifeSprite") as Sprite2D;
+		knifeSprite.Texture = knifeWithButter;
+	}
+	
+
+}
+
+
+
+
+
+
