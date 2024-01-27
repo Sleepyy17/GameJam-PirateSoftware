d4af626eb10dee8de01ac23eab187884d603d3da
brian489 <brian.nguyen292@gmail.com>
2024-01-27 11:43:28 +1100

    added buttons to main menu, fixed issue with level 4, changed level 3

--- Scripts/EndNode.cs
+++ Scripts/EndNode.cs
@@ -16,23 +16,25 @@ public partial class EndNode : Node2D
        if (tree.Paused == true) {
         tree.Paused = false;
        }
-    //    PlayerCamera2D = GetNode<Camera2D>("../Knife/Camera2D");
+
        WinScreen = GetNode<Control>("../Node2D/WinScreen");
-       GD.Print(PlayerCamera2D);
+
        GD.Print(WinScreen);
        GD.Print(tree);
     }
 
     public void _on_area_2d_area_entered(Area2D area) {
         if (area.Name == "BladeArea") {
-            GD.Print(name);
+            
             if (name == "LevelOne") {
                 g.LevelOneComplete = true;
             } else if (name == "LevelTwo") {
                 g.LevelTwoComplete = true;
             } else if (name == "LevelThree") {
                 g.LevelThreeComplete = true;
-            } 
+            } else if (name == "LevelFour") {
+                g.LevelFourComplete = true;
+            }
             WinScreen.Visible = true;
             tree.Paused = true;
         }
